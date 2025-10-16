2deacfafc06468871965c6c2f0f013aeafa1940d
Your Name <you@example.com>
2025-10-16 15:33:06 +0800

    2

--- src/main.ts
+++ src/main.ts
@@ -81,15 +81,15 @@ async function getAllPanelBlockIds() {
   console.log("所有面板的块ID:", blockIds);
   
   // 读取所有面板的容器块元素
-  readAllPanelsContainerBlocks(viewPanels);
+  await readAllPanelsContainerBlocks(viewPanels);
   
   return blockIds;
 }
 
 /**
- * 读取所有面板中的容器块 data-id，并筛选出带标签的块
+ * 读取所有面板中的容器块 data-id，并筛选出带标签的块及其第一个标签名和别名块ID
  */
-function readAllPanelsContainerBlocks(viewPanels: any[]) {
+async function readAllPanelsContainerBlocks(viewPanels: any[]) {
   for (const panel of viewPanels) {
     const panelId = panel.id;
     
@@ -103,8 +103,9 @@ function readAllPanelsContainerBlocks(viewPanels: any[]) {
     // 在该面板内查询所有容器块元素
     const containerElements = panelElement.querySelectorAll('.orca-block.orca-container');
     
-    // 筛选出带标签的容器块
-    const taggedBlockIds: string[] = [];
+    // 筛选出带标签的容器块，并获取第一个标签名
+    const taggedBlocksPromises: Promise<{ blockId: string; firstTag: string; aliasBlockId: number | null }>[] = [];
+    
     containerElements.forEach((element) => {
       // 查找该容器块下的 .orca-repr-main 元素
       const reprMainElement = element.querySelector('.orca-repr-main');
@@ -114,18 +115,45 @@ function readAllPanelsContainerBlocks(viewPanels: any[]) {
         const tagsElement = reprMainElement.querySelector('.orca-tags');
         
         if (tagsElement) {
-          // 如果有标签，记录这个容器块的 data-id
-          const dataId = element.getAttribute('data-id');
-          if (dataId) {
-            taggedBlockIds.push(dataId);
+          // 获取第一个 .orca-tag 元素
+          const firstTagElement = tagsElement.querySelector('.orca-tag');
+          
+          if (firstTagElement) {
+            const dataId = element.getAttribute('data-id');
+            const firstTagName = firstTagElement.getAttribute('data-name');
+            
+            if (dataId && firstTagName) {
+              // 异步获取别名块ID
+              const promise = (async () => {
+                try {
+                  const result = await orca.invokeBackend("get-blockid-by-alias", firstTagName);
+                  return {
+                    blockId: dataId,
+                    firstTag: firstTagName,
+                    aliasBlockId: result?.id ?? null
+                  };
+                } catch (error) {
+                  return {
+                    blockId: dataId,
+                    firstTag: firstTagName,
+                    aliasBlockId: null
+                  };
+                }
+              })();
+              
+              taggedBlocksPromises.push(promise);
+            }
           }
         }
       }
     });
     
+    // 等待所有异步操作完成
+    const taggedBlocks = await Promise.all(taggedBlocksPromises);
+    
     // 只输出有带标签容器块的面板
-    if (taggedBlockIds.length > 0) {
-      console.log(`当前面板 [${panelId}] 的带标签容器块 data-id:`, taggedBlockIds);
+    if (taggedBlocks.length > 0) {
+      console.log(`当前面板 [${panelId}] 的带标签容器块:`, taggedBlocks);
     }
   }
 }
